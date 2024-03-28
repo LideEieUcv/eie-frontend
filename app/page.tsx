f341caa8e74afc00ef09495fd3e1d4cde5a1a1e2
ale <alejandroxh14@gmail.com>
2024-03-28 02:31:47 -0400

    feat: create basic frontend structure

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="">
+        <p className="">
+          Escuela de Ingeniería Eléctrica UCV
+        </p>
+      </div>
+    </main>
+  );
+}

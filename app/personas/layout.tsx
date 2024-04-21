cd8df20168985ab06091b1e5a72af9fc1570032d
carlosjmarq <carlosjmarq@gmail.com>
2024-04-21 11:18:18 -0400

    feat: people page first draft

--- app/personas/layout.tsx
+++ app/personas/layout.tsx
@@ -7,7 +7,7 @@ const PeopleLayout = ({
     <div className="w-full">
       {/* TODO: component de header */}
       <header className="flex h-[125px] w-full bg-slate-300">
-        <h1 className="m-auto text-3xl font-bold">Personas</h1>
+        <h1 className="m-auto text-4xl font-bold">Personas</h1>
       </header>
       {children}
     </div>
--- app/personas/page.tsx
+++ app/personas/page.tsx
@@ -1,3 +1,107 @@
 export default function Page() {
-    return <p>Personas</p>;
-  }
+  return (
+    <div className="grid-cols-1/5  mx-40 mt-12 grid">
+      {/* Componente de navegacion lateral */}
+      <ul className="flex h-fit flex-col border-r border-black pb-2">
+        <li className="px-2 py-2 hover:bg-slate-100">Profesores</li>
+        <li className="px-2 py-2 hover:bg-slate-100">Egresados</li>
+        <li className="px-2 py-2 hover:bg-slate-100">Administrativo</li>
+      </ul>
+      <main className="px-2 pl-4">
+        <h3 className="mb-4 text-4xl font-bold">Profesores</h3>
+        <div className="w-full border border-slate-600">Barra busqueda</div>
+        <div className="flex w-full items-center justify-between">
+          <p>58 profesores</p>{' '}
+          <div className="flex items-center justify-between gap-4">
+            <p>Organizar</p> <div className="border border-slate-600 px-2 py-1">A-Z ^</div>
+          </div>
+        </div>
+        <section className="grid grid-flow-row grid-cols-5 gap-4">
+          <article className="">
+            <div className="h-[180px] w-full bg-slate-300"></div>
+            <h5>Jhon doe</h5>
+            <p>
+              Lorem ipsum dolor sit amet consectetur. Diam vitae tellus est pellentesque. Sed
+              suspendisse aenean suscipit vulputate. Aliquam diam at amet velit posuere eget. Non
+              purus elit eleifend lectus mauris enim non.
+            </p>
+          </article>
+
+          <article className="">
+            <div className="h-[180px] w-full bg-slate-300"></div>
+            <h5>Jhon doe</h5>
+            <p>
+              Lorem ipsum dolor sit amet consectetur. Diam vitae tellus est pellentesque. Sed
+              suspendisse aenean suscipit vulputate. Aliquam diam at amet velit posuere eget. Non
+              purus elit eleifend lectus mauris enim non.
+            </p>
+          </article>
+
+          <article className="">
+            <div className="h-[180px] w-full bg-slate-300"></div>
+            <h5>Jhon doe</h5>
+            <p>
+              Lorem ipsum dolor sit amet consectetur. Diam vitae tellus est pellentesque. Sed
+              suspendisse aenean suscipit vulputate. Aliquam diam at amet velit posuere eget. Non
+              purus elit eleifend lectus mauris enim non.
+            </p>
+          </article>
+          <article className="">
+            <div className="h-[180px] w-full bg-slate-300"></div>
+            <h5>Jhon doe</h5>
+            <p>
+              Lorem ipsum dolor sit amet consectetur. Diam vitae tellus est pellentesque. Sed
+              suspendisse aenean suscipit vulputate. Aliquam diam at amet velit posuere eget. Non
+              purus elit eleifend lectus mauris enim non.
+            </p>
+          </article>
+          <article className="">
+            <div className="h-[180px] w-full bg-slate-300"></div>
+            <h5>Jhon doe</h5>
+            <p>
+              Lorem ipsum dolor sit amet consectetur. Diam vitae tellus est pellentesque. Sed
+              suspendisse aenean suscipit vulputate. Aliquam diam at amet velit posuere eget. Non
+              purus elit eleifend lectus mauris enim non.
+            </p>
+          </article>
+          <article className="">
+            <div className="h-[180px] w-full bg-slate-300"></div>
+            <h5>Jhon doe</h5>
+            <p>
+              Lorem ipsum dolor sit amet consectetur. Diam vitae tellus est pellentesque. Sed
+              suspendisse aenean suscipit vulputate. Aliquam diam at amet velit posuere eget. Non
+              purus elit eleifend lectus mauris enim non.
+            </p>
+          </article>
+          <article className="">
+            <div className="h-[180px] w-full bg-slate-300"></div>
+            <h5>Jhon doe</h5>
+            <p>
+              Lorem ipsum dolor sit amet consectetur. Diam vitae tellus est pellentesque. Sed
+              suspendisse aenean suscipit vulputate. Aliquam diam at amet velit posuere eget. Non
+              purus elit eleifend lectus mauris enim non.
+            </p>
+          </article>
+          <article className="">
+            <div className="h-[180px] w-full bg-slate-300"></div>
+            <h5>Jhon doe</h5>
+            <p>
+              Lorem ipsum dolor sit amet consectetur. Diam vitae tellus est pellentesque. Sed
+              suspendisse aenean suscipit vulputate. Aliquam diam at amet velit posuere eget. Non
+              purus elit eleifend lectus mauris enim non.
+            </p>
+          </article>
+          <article className="">
+            <div className="h-[180px] w-full bg-slate-300"></div>
+            <h5>Jhon doe</h5>
+            <p>
+              Lorem ipsum dolor sit amet consectetur. Diam vitae tellus est pellentesque. Sed
+              suspendisse aenean suscipit vulputate. Aliquam diam at amet velit posuere eget. Non
+              purus elit eleifend lectus mauris enim non.
+            </p>
+          </article>
+        </section>
+      </main>
+    </div>
+  );
+}

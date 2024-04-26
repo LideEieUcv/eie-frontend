178088ce6113cdd7b768434fef542b2f093df01c
Jose Lopez <joseglopez85@gmail.com>
2024-04-26 00:52:19 -0400

    Feat: Moving components in footer

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -6,10 +6,29 @@ interface FooterProps {
 
 const Footer: React.FC<FooterProps> = ({}) => {
   return (
-    <footer className="bg-gray-800 text-white p-4 flex justify-center items-center w-full bottom-0 fixed">
-      <p className="text-xs">Copyright &copy; {new Date().getFullYear()}</p>
-      <a href="#" className="text-xs hover:text-gray-300 ml-4">Terms & Conditions</a>
-      <a href="#" className="text-xs hover:text-gray-300 ml-4">Privacy Policy</a>
+    <footer className="bg-gray-100 text-black p-4 flex flex-col justify-center items-center w-full bottom-0 fixed">
+      {/* Section 1: Links */}
+      <div className="flex flex-row gap-4 items-center justify-center w-full">
+        <a href="#" className="text-xs hover:text-gray-300">Pregado</a>
+        <a href="#" className="text-xs hover:text-gray-300">Postgrado</a>
+        <a href="#" className="text-xs hover:text-gray-300">Contacto</a>
+      </div>
+
+      {/* Section 2: Divider */}
+      <div className="border-t border-gray-700 w-full my-4"></div>
+
+      {/* Section 3: Logos */}
+      <div className="flex flex-row gap-4 items-center justify-between w-full">
+        <div className="flex flex-col items-center">
+          <a href="/" className="text-gray-800 border border-black font-bold text-md">LOGO UCV</a>
+        </div>
+        <div className="flex flex-col items-center">
+          <a href="/" className="text-gray-800 border border-black font-bold text-md">LOGO UCV ING.</a>
+        </div>
+        <div className="flex flex-col items-center">
+          <a href="/" className="text-gray-800 border border-black font-bold text-md">LOGO UCV ELEC.</a>
+        </div>
+      </div>
     </footer>
   );
 };

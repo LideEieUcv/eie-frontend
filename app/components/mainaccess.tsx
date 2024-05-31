a6bbc25550ae1266191bd48af8cfd2eceeec645b
Jose Lopez <joseglopez85@gmail.com>
2024-05-30 22:46:21 -0400

    Adding hover a elements

--- app/components/mainaccess.tsx
+++ app/components/mainaccess.tsx
@@ -9,7 +9,7 @@ interface BoxProps {
     return (
       <div className="border-2 border-gray-500 bg-gray-600 p-4 relative w-[350px] h-64">
         <img src={image} alt="Imagen" className="w-32 h-32 object-cover" />
-        <a href="#" className="inline-block mt-10 p-2 text-white text-xl flex flex-shrink flex-grow object-fit">
+        <a href="#" className="mt-10 p-2 text-white text-xl flex flex-shrink flex-grow object-fit">
           {title} →
         </a>
       </div>

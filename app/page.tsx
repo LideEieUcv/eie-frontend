f0da595acd9168a216516b22ad7ae9dafd4ea5e2
Jose Lopez <joseglopez85@gmail.com>
2024-10-20 16:29:51 -0400

    fix: updating title the section

--- app/page.tsx
+++ app/page.tsx
@@ -11,17 +11,25 @@ const eventsData = [
   // Agrega más eventos si es necesario
 ];
 
-
 const Index = () => {
   return (
-    <div className="container mx-auto py-20 w-screen">
+    <div className="container mx-auto py-20">
       <Navbar />
-      <h1 className='text-3xl font-bold my-16'><center>ESCUELA DE INGENIERIA ELECTRICA UCV</center></h1>
+      <h1 className='text-3xl font-bold mt-16'><center>ESCUELA DE INGENIERIA ELECTRICA UCV</center></h1>
       <h2 className='my-5 font-medium'><center>Bienvenido a la Escuela de Ingeniería Eléctrica de la Facultad de Ingeniería. Como parte de la <br></br>política e integración de la información UCV, estamos realizando la migración de nuestro<br></br> sitio web al nuevo Portal.</center></h2>
-      <div className= "flex flex-row justify-center py-4 gap-x-4">
+      <div className= "flex flex-row justify-center items-center py-4 gap-x-5 ">
         <Box image={''} title={'Pregrado'}/>
         <Box image={''} title={'Posgrado'}/>
         <Box image={''} title={'Nuevos ingresos'}/>
+      </div>
+
+      <div className='mt-10 flex flex-row justify-center items-center'>
+        <div className='justify-start mr-40'>
+          <h1 className='font-extrabold text-3xl'>Ultimas noticias</h1>
+        </div>
+        <div className='justify-end'>
+          <a href="/noticias-y-eventos" className='font-bold text-md bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 transition'>Mas noticias →</a>
+        </div>
       </div>
       <div className='justify-center flex flex-row mt-16 mb-16'>
         <Calendar events={eventsData} />

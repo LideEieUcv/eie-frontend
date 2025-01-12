d791da683bd7b81f3e94838dc581ef70f42735a6
Jose Lopez <joseglopez85@gmail.com>
2025-01-11 20:14:08 -0400

    fix:finish four section the home page

--- app/page.tsx
+++ app/page.tsx
@@ -87,29 +87,29 @@ const Index = () => {
 
       {/* Cuarta seccion */}
       <div className='flex flex-row justify-center items-center min-h-96 bg-gray-50 text-center text-black'>
-        <div className='flex flex-col items-start w-1/2 gap-y-5'>
+        <div className='flex-col items-start space-y-5 px-24 w-1/2 hidden md:block'>
           <h1 className='text-lg font-bold text-left ml-20'>Ciclo de inscripciones<br></br>2024-2025</h1>
           <p className='text-sm font-medium text-left ml-20'> Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fuga, nihil similique. Aut sit fuga maiores voluptatibus iste suscipit corporis nesciunt tempore praesentium rerum facilis accusantium amet totam ullam, a commodi!</p>
         </div>
-          <div className='flex flex-col items-start p-20 w-1/2'>  
-            <div className='flex flex-col items-center justify-center text-center gap-y-6 w-96'>  
-                <a   
-                    href="/"   
-                    className='w-full font-bold border-2 border-black text-md text-black px-4 py-2 hover:text-gray-400 transition text-center'>  
-                    Calendario 2024 →  
-                </a>  
-                <a   
-                    href="/"   
-                    className='w-full font-bold border-2 border-black text-md text-black px-4 py-2 hover:text-gray-400 transition text-center'>  
-                    Recaudos de Inscripción →  
-                </a>  
-                <a   
-                    href="/"   
-                    className='w-full font-bold border-2 border-black text-md text-black px-4 py-2 hover:text-gray-400 transition text-center'>  
-                    Programa de estudio →  
-                </a>  
-            </div>  
-        </div>  
+        <div className='flex justify-center items-center p-20'>  
+          <div className='flex flex-col items-center justify-center text-center gap-y-6 w-96'>  
+            <a   
+              href="/"   
+              className='w-full font-bold border-2 border-black text-md text-black px-4 py-2 hover:text-gray-400 transition text-center'>  
+              Calendario 2024 <span className='ml-28'>→</span>  
+            </a>  
+            <a   
+              href="/"   
+              className='w-full font-bold border-2 border-black text-md text-black px-4 py-2 hover:text-gray-400 transition text-center'>  
+              Recaudos de Inscripción <span className='ml-12'>→</span>  
+            </a>  
+            <a   
+              href="/"   
+              className='w-full font-bold border-2 border-black text-md text-black px-4 py-2 hover:text-gray-400 transition text-center'>  
+              Programa de estudio <span className='ml-20'>→</span>  
+            </a>  
+          </div>  
+        </div>   
       </div>
 
     </>

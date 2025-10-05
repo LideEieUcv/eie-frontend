dbd12afdfa02375497f6204c09833bed78c54434
Jose Lopez <joseglopez85@gmail.com>
2025-10-05 11:49:06 -0400

    fix: update design header

--- app/informacion-academica/page.tsx
+++ app/informacion-academica/page.tsx
@@ -6,7 +6,7 @@ const Index = () => {
   return (  
     <>  
       {/* Título principal */}  
-      <div className='min-h-96 bg-gray-300 mb-5 flex flex-col items-center justify-center text-black text-2xl md:text-3xl lg:text-4xl font-extrabold text-center px-4'>  
+      <div className='min-h-96 bg-gradient-to-r from-blue-500 to-purple-600 mb-5 flex flex-col items-center justify-center text-black text-2xl md:text-3xl lg:text-4xl font-extrabold text-center px-4'>  
         <h1>INFORMACION ACADEMICA</h1>  
       </div>  
 
